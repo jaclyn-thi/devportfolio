aff36efa9fda7c9d702c828b7c4f9d4419d85256
Jaclyn Thi <jthi@mit.edu>
2026-05-22 18:46:41 -0400

    Configuring github

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'jaclyn-thi.github.io',
+  base: '/devportfolio',
   vite: {
     plugins: [tailwindcss()],
   },
